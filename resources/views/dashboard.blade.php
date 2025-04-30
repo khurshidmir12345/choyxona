b8c28473bc25382d6a56b16600fd5f7c8605c4d3
khurshidmir12345 <kubilaymirzajonov@gmail.com>
2025-04-30 10:06:22 +0300

    moonshine installed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
+            <div>{{auth()->user()->phone_number}}</div>
         </h2>
     </x-slot>
 
