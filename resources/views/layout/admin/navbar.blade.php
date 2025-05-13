5c23c8df1c34f11cf6b75e4b0cf9bb50d930cbe2
zuxyy <xolixmatovzuxriddin@gmail.com>
2025-05-13 11:09:01 +0500

    fixed login page

--- resources/views/layout/admin/navbar.blade.php
+++ resources/views/layout/admin/navbar.blade.php
@@ -157,7 +157,14 @@
                     <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i> Messages</a>
                     <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i> Activity</a>
                     <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2"></i> FAQ</a>
-                    <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</a>
+                    <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none;">
+                        @csrf
+                    </form>
+
+                    <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out
+                    </a>
+
                 </div>
             </li>
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ Route::get('/home', function () {
 })->middleware('auth')->name('home');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
